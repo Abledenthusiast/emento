b3d5840584921ee3b6b322959b56932e31b7098a
abledenthusiast <pitmana@xavier.edu>
2019-12-07 18:01:03 -0800

    first commit

---
+++ server/src/main/java/EmentoApplication.java
@@ -0,0 +1,9 @@
+
+import io.micronaut.runtime.Micronaut;
+
+public class EmentoApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(EmentoApplication.class);
+    }
+}
