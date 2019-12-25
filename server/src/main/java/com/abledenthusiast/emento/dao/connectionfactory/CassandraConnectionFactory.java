0c3d6721b233e59743e560c7db48435f9d87c66e
abledenthusiast <pitmana@xavier.edu>
2019-12-25 01:23:19 -0500

    add ssl

--- server/src/main/java/com/abledenthusiast/emento/dao/connectionfactory/CassandraConnectionFactory.java
+++ server/src/main/java/com/abledenthusiast/emento/dao/connectionfactory/CassandraConnectionFactory.java
@@ -1,14 +1,25 @@
 package com.abledenthusiast.emento.dao.connectionfactory;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ProtocolVersion;
-import com.datastax.driver.core.Session;
+import com.datastax.driver.core.*;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
 
 public class CassandraConnectionFactory implements ConnectionFactory<Session> {
     private String host;
     private int port;
     private String username;
     private String password;
+
+    private String sslKeyStorePassword;
+    private String ssl_keystore_file_path;
+
+    private File sslKeyStoreFile;
 
     private Cluster cluster;
 
@@ -22,6 +33,31 @@ public class CassandraConnectionFactory implements ConnectionFactory<Session> {
     @Override
     public Session connect() {
         if (cluster == null) {
+            try {
+                loadCassandraConnectionDetails();
+
+                final KeyStore keyStore = KeyStore.getInstance("JKS");
+                try (final InputStream is = new FileInputStream(sslKeyStoreFile)) {
+                    keyStore.load(is, sslKeyStorePassword.toCharArray());
+                }
+
+                final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory
+                                                                                .getDefaultAlgorithm());
+                kmf.init(keyStore, sslKeyStorePassword.toCharArray());
+                final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory
+                                                                                    .getDefaultAlgorithm());
+                tmf.init(keyStore);
+
+                // Creates a socket factory for HttpsURLConnection using JKS contents.
+                final SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new java.security.SecureRandom());
+
+                JdkSSLOptions sslOptions = RemoteEndpointAwareJdkSSLOptions.builder()
+                                                                           .withSSLContext(sc)
+                                                                           .build();
+            } catch(Exception e) {
+
+            }
             cluster = Cluster.builder()
                              .withProtocolVersion(ProtocolVersion.V4)
                              .addContactPoint(host)
@@ -31,5 +67,31 @@ public class CassandraConnectionFactory implements ConnectionFactory<Session> {
         }
 
         return cluster.connect();
+    }
+
+    /**
+     * Loads Cassandra end-point details from config.properties.
+     * @throws Exception
+     */
+    private void loadCassandraConnectionDetails() throws Exception {
+        String ssl_keystore_file_path;
+        String ssl_keystore_password = null;
+
+        // If ssl_keystore_file_path, build the path using JAVA_HOME directory.
+
+        String javaHomeDirectory = System.getenv("JAVA_HOME");
+        if (javaHomeDirectory == null || javaHomeDirectory.isEmpty()) {
+            throw new Exception("JAVA_HOME not set");
+        }
+        ssl_keystore_file_path = new StringBuilder(javaHomeDirectory).append("/jre/lib/security/cacerts").toString();
+
+        sslKeyStorePassword = (ssl_keystore_password != null && !ssl_keystore_password.isEmpty()) ?
+                              ssl_keystore_password : sslKeyStorePassword;
+
+        sslKeyStoreFile = new File(ssl_keystore_file_path);
+
+        if (!sslKeyStoreFile.exists() || !sslKeyStoreFile.canRead()) {
+            throw new Exception(String.format("Unable to access the SSL Key Store file from %s", ssl_keystore_file_path));
+        }
     }
 }
